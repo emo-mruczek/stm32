be4594eae59a7bf26a0cba96383d25d69bc93d9d
emo-mruczek <krokcia1@gmail.com>
2024-08-25 01:26:55 +0200

    lcd: sending data

--- LCD_1602_write/main.c
+++ LCD_1602_write/main.c
@@ -1,6 +1,6 @@
 /* with a help of 
  * https://controllerstech.com/interface-lcd-16x2-with-stm32-without-i2c/
- * https://deepbluembedded.com/stm32-lcd-16x2-tutorial-library-alphanumeric-lcd-16x2-interfacing/  * and some random LCD1602 datasheet */
+ * https://deepbluembedded.com/stm32-lcd-16x2-tutorial-library-alphanumeric-lcd-16x2-interfacing/  * and some random LCD1602 datasheet, as i bought my lcd on aliexpress */
 
 /* this is a gpio hell... i need to clean it up! */
 
@@ -24,13 +24,76 @@
 void configure_clock(void);
 void configure_gpios(void);
 void start_timer(uint16_t ms);
+void stop_timer();
 void delay(uint16_t ms);
-void to_lcd(char data, char type);
+void send_to_lcd(char data, char type);
+void send_nibble(char data, char type);
+void initialize_lcd(void);
 
 int main(void) {
 
     configure_clock();
     configure_gpios();
+}
+
+void initialize_lcd(void) {
+    // taken from datasheet 
+    
+    delay(45);
+    send_to_lcd(0x30, send_command);
+    delay(45);
+    send_to_lcd(0x30, send_command);
+    delay(2);
+    send_to_lcd(0x30, send_command);
+    delay(10);
+
+    // 4-bit mode
+    send_to_lcd(0x20, send_command);
+    delay(10);
+
+    // 
+
+
+
+}
+
+void put_cursor(char row, char col) {
+    
+    // "set DDRAM address or cursor position on display 0x80 + add"
+    // "80 Force cursor to the beginning (1st line)"
+    // "C0 Force cursor to the beginning (2nd line)"
+    
+    col |= ( row ?  0x80 : 0xC0 );
+
+    send_to_lcd(col, send_command);
+}
+
+// okay so in 4-bit mode i need to send data in nibbles, thus this function
+void send_to_lcd(char data, char type) {
+
+    // 0000xxxx & 1111
+    send_nibble( (data >> 4) & 0x0f, type );
+    send_nibble( data & 0x0f, type);
+}
+
+// char looks ok to use
+void send_nibble(char data, char type) {
+
+    // command or data?
+    type ? RS_set : RS_reset;
+
+    // set/reset pins
+    ( data >> 0 & 1 ) ? D4_set : D4_reset;
+    ( data >> 1 & 1 ) ? D5_set : D5_reset;
+    ( data >> 2 & 1 ) ? D6_set : D6_reset;
+    ( data >> 3 & 1 ) ? D7_set : D7_reset;
+
+    // enable tick
+    // delay should be ok, my clock speed is not very high
+    EN_set;
+    delay(20);
+    EN_reset;
+    delay(20);
 }
 
 void delay(uint16_t ms) {
@@ -39,6 +102,7 @@ void delay(uint16_t ms) {
 
     // so im waiting for interrupt, basically
     while (!(TIM3->SR & TIM_SR_UIF)) {};
+    stop_timer();
     TIM3->SR &= ~TIM_SR_UIF; // need to reset it manualy
 }
 
@@ -68,23 +132,9 @@ void start_timer(uint16_t ms) {
     TIM3->CR1 |= TIM_CR1_CEN; // counter enable 
 }
 
-// char looks ok to use
-void to_lcd(char data, char type) {
-
-    // command or data?
-    type ? RS_set : RS_reset;
-
-    // set/reset pins
-    ( data >> 0 & 1 ) ? D4_set : D4_reset;
-    ( data >> 1 & 1 ) ? D5_set : D5_reset;
-    ( data >> 2 & 1 ) ? D6_set : D6_reset;
-    ( data >> 3 & 1 ) ? D7_set : D7_reset;
-
-    // enable tick
-    EN_set;
-    delay(20);
-    EN_reset;
-    delay(20);
+void stop_timer() {
+    TIM3->CR1 &= ~TIM_CR1_CEN;
+    TIM3->SR &= ~TIM_SR_UIF;
 }
 
 // what a monstrosity 
