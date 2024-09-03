0bad04e5a2793b7e71d677a7b03caf9828562635
emo-mruczek <krokcia1@gmail.com>
2024-09-03 14:56:15 +0200

    not-really-worky dh

--- DH11/main.c
+++ DH11/main.c
@@ -1,6 +1,10 @@
 /* this is going to be a program that would read temperature value from dh11 */
 /* for now, i will use LEDs to show output */
 
+/* its not worky rn but i will fix it! */
+
+//TODO: debug via one external led
+
 #include "stm32f103x6.h"
 
 #define bit_set_0   ( GPIOA->BSRR = GPIO_BSRR_BS12 )
@@ -20,11 +24,13 @@
 void print_value(uint8_t value);
 void initialize_gpios(void);
 void configure_clock(void);
-void delay(uint16_t ms);
+void delay(uint16_t us);
 void initialize_debug(void);
 void run_debug();
+uint8_t read_byte_response(void);
 
 volatile uint8_t debug = 0;
+// TODO: better timer handler
 volatile uint32_t ticks = 0;
 
 int main() {
@@ -32,12 +38,14 @@ int main() {
     initialize_gpios();
     initialize_debug(); 
 
-    if ( debug ) run_debug();
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
 
+
+    // TODO: make more functions
     // output for initialization
     GPIOB->CRH &= ~GPIO_CRH_MODE12_Msk;
     GPIOB->CRH |= GPIO_CRH_MODE12_1;
-    GPIOB->CRH &= ~GPIO_CRH_CNF_Msk;
+    GPIOB->CRH &= ~GPIO_CRH_CNF12_Msk;
 
     /* "Data Single-bus free status is at high voltage level. 
      * When the communication between MCU and
@@ -46,15 +54,64 @@ int main() {
      * then MCU will pull up voltage and wait 20-40us for DHT’s response" */
 
     DHT_reset;
-    delay(20000);
+    delay(18000);
     DHT_set;
-    delay(40);
+    delay(30);
 
     // changing to input mode to receive data
-    GPIOB->CRH &= ~GPIO_CRH_MODE12_Msk; 
-    // cnf already set to input with pull-up/pull-down
-    DHT_reset;
+    GPIOB->CRH &= ~GPIO_CRH_MODE12_Msk;
+    GPIOB->CRH |= GPIO_CRH_CNF12_1; 
+    GPIOB->ODR |= GPIO_ODR_ODR12;      
 
+    // checking for the response -> should be puled to low for approx 80us by dht
+    uint32_t ticks_after_delay = ticks + 80;
+    while( !(GPIOB->IDR & GPIO_IDR_IDR12) && (ticks < ticks_after_delay) ) __asm__("nop");
+   // if (ticks > ticks_after_delay) {
+       // print_value(63);
+   //     return 0;
+   // }
+
+    // ...aaand pulled up
+    ticks_after_delay = ticks + 100;
+    while ( (GPIOB->IDR & GPIO_IDR_IDR12) && (ticks < ticks_after_delay) ) __asm__("nop");
+   // if (ticks > ticks_after_delay) {
+       // print_value(63);
+   //     return 0;
+   // }
+
+
+    // now, collect the data
+    /* "Data format:
+     * 8bit integral RH data + 8bit decimal RH data + 8bit integral T data + 8bit decimal T data + 8bit check sum." */
+
+    /* When DHT is sending data to MCU, every bit of data begins with the 50us low-voltage-level and
+     * the length of the following high-voltage-level signal determines whether data bit is "0" or "1" */
+
+    // there is a problem with reading
+    uint8_t RH_int = read_byte_response();
+    uint8_t RH_dec = read_byte_response();
+    uint8_t T_int = read_byte_response();
+    uint8_t T_dec = read_byte_response();
+    uint8_t checksum = read_byte_response();
+
+    // TODO: checksum check
+
+    print_value(T_int);
+}
+
+uint8_t read_byte_response(void) {
+    // it starts from highest bit
+    uint8_t res = 0;
+    
+    for ( uint8_t i = 0; i < 8; ++i ) {
+        while( !(GPIOB->IDR & GPIO_IDR_IDR12) ) {};
+        uint32_t prev_ticks = ticks;
+        while( (GPIOB->IDR & GPIO_IDR_IDR12) ) {};
+        if ( ticks - prev_ticks >= 40 ) res |= ( 1 << (7 - i) );
+        if ( debug ) run_debug();
+    }
+
+    return res;
 }
 
 
@@ -102,7 +159,7 @@ void run_debug(void) {
     if ( debug ) {
         for ( uint8_t i = 0; i < 4; ++i ) {
             GPIOC->ODR ^= GPIO_ODR_ODR13;
-            delay(100);
+            delay(40000);
         }
     }
 }
